670e86f1a5f36aa4126aef087fada7bc8c0a823b
Christian Holl <cyborgx1@gmail.com>
2017-03-11 09:31:12 +0100

    read conversion, cause obj is currently not really supported :-/
    WTF, conversion does not work anymore ?!?

--- scripts/blender_convert.py
+++ scripts/blender_convert.py
@@ -152,8 +152,52 @@ def analyze_obj(obj_path, out_path, dimensions=None):
     
     
     
+    createDir(out_path+"/"+file_name)
+    ret_data['model_file']="/"+file_name+"/"+file_name+".dae"
+    out_file=out_path+ret_data['model_file']
     
-        
+    
+    print("Converted Model: ", obj_path + " to " + out_file)
+    
+    
+    #Export to collada file
+    bpy.ops.wm.collada_export(out_file,
+                              check_existing=True, 
+                              filter_blender=False, 
+                              filter_backup=False, 
+                              filter_image=False, 
+                              filter_movie=False, 
+                              filter_python=False, 
+                              filter_font=False, 
+                              filter_sound=False, 
+                              filter_text=False, 
+                              filter_btx=False, 
+                              filter_collada=True, 
+                              filter_alembic=False,
+                              filter_folder=True, 
+                              filter_blenlib=False, 
+                              filemode=8, 
+                              display_type='DEFAULT', 
+                              sort_method='FILE_SORT_ALPHA', 
+                              apply_modifiers=False, 
+                              export_mesh_type=0, 
+                              export_mesh_type_selection='view', 
+                              selected=False, 
+                              include_children=False, 
+                              include_armatures=False, 
+                              include_shapekeys=True, 
+                              deform_bones_only=False, 
+                              active_uv_only=False, 
+                              include_uv_textures=True, 
+                              include_material_textures=True, 
+                              use_texture_copies=True, 
+                              triangulate=True, 
+                              use_object_instantiation=True, 
+                              use_blender_profile=True, 
+                              sort_by_name=False, 
+                              export_transformation_type=0, 
+                              export_transformation_type_selection='matrix', 
+                              open_sim=False)  
     return ret_data;
 
 
